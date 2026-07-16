32781599afc86027accbf6fcd58f4341d8aac4df
Victor Hugo Germano <victorhg@gmail.com>
2026-07-16 09:19:09 -0300

    included neogit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,29 @@ return {
 
   { 
     "carderne/pi-nvim"
-    }
+    },
 
+
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      -- Only one of these is needed.
+      "sindrets/diffview.nvim",        -- optional
+      "esmuellert/codediff.nvim",      -- optional
+
+      -- For a custom log pager
+      "m00qek/baleia.nvim",            -- optional
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "nvim-mini/mini.pick",           -- optional
+      "folke/snacks.nvim",             -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  }
 }
