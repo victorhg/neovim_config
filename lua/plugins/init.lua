4ea6c75953f6d081d0f288b1310e5069c2d58a18
Victor Hugo Germano <victorhg@gmail.com>
2026-08-03 07:48:50 -0300

    render markdown

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,13 @@ return {
   --
 
   {
+     'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
     "carderne/pi-nvim"
   },
 
